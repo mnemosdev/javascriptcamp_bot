c90cf284942afc96ccc2353df81de5897a3dda9c
Playra <raoffonom@icloud.com>
2021-03-24 12:59:29 +0500

    en

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 /* eslint-disable indent */
 require('dotenv').config()
 const { Telegraf, session, Stage, BaseScene } = require('telegraf')
-// const TelegrafI18n = require('telegraf-i18n')
+const TelegrafI18n = require('telegraf-i18n')
 const { level, getSticker, MyContext } = require('./helpers')
-const questions = require('./questions')
-// const path = require('path')
+const ru = require('./quiz/ru')
+const en = require('./quiz/en')
+const path = require('path')
 
-// const i18n = new TelegrafI18n({
-//   defaultLanguage: 'en',
-//   allowMissing: false, // Default true
-//   directory: path.resolve(__dirname, 'locales')
-// })
+const i18n = new TelegrafI18n({
+  defaultLanguage: 'en',
+  directory: path.resolve(__dirname, 'locales')
+})
 
-const bot = new Telegraf(process.env.BOT_TOKEN, { contextType: MyContext })
+let BOT_TOKEN
+if (process.env.NODE_ENV === 'production') {
+  BOT_TOKEN = process.env.BOT_TOKEN
+} else {
+  BOT_TOKEN = process.env.BOT_TOKEN_TEST
+}
+const bot = new Telegraf(BOT_TOKEN, { contextType: MyContext })
+
+bot.use(i18n.middleware())
 
 const jsRoom = new BaseScene('js-room')
 
+let getQuiz = ctx => (ctx.i18n.locale() === 'en' ? en : ru)
+
+let questions
+
 jsRoom.enter(ctx => {
+  questions = getQuiz(ctx)
   const questionIndex = 0
   const counter = 0
   ctx.session.counter = counter
@@ -24,7 +37,7 @@ jsRoom.enter(ctx => {
 
   const { title, random, correct_option_id } = questions[questionIndex]
 
-  ctx.replyWithQuiz(`Вопрос: 1 из ${questions.length}\n${title}`, random, {
+  ctx.replyWithQuiz(`${ctx.i18n.t('Question')}: 1 ${ctx.i18n.t('from')} ${questions.length}\n${title}`, random, {
     correct_option_id,
     is_anonymous: false
   })
@@ -37,10 +50,14 @@ jsRoom.on('poll_answer', ctx => {
 
   if (questionIndex !== questions.length) {
     const { title, random, correct_option_id } = questions[questionIndex]
-    return ctx.replyWithQuiz(`Вопрос: ${questionIndex + 1} из ${questions.length}\n${title}`, random, {
-      correct_option_id,
-      is_anonymous: false
-    })
+    return ctx.replyWithQuiz(
+      `${ctx.i18n.t('Question')}: ${questionIndex + 1} ${ctx.i18n.t('from')} ${questions.length}\n${title}`,
+      random,
+      {
+        correct_option_id,
+        is_anonymous: false
+      }
+    )
   } else {
     ctx.reply(
       `Ваш счет: ${ctx.session.counter}. Уровень: ${level(ctx.session.counter)} ${getSticker(ctx.session.counter)}`
